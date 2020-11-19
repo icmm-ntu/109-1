dd6fc89038be933402413fc8d5bf93be5bdadd9c
icmm-ntu <74669310+icmm-ntu@users.noreply.github.com>
2020-11-19 14:25:13 +0800

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -6,8 +6,7 @@ x_arr = np.linspace(0,35,1001)
 
 plt.plot(x_arr, np.exp(x_arr),'-k')
 x1 = 0.1
-x2 = x1 + 10 *np.pi
-plt.plot(x1, np.exp(x1),'ro')
-plt.plot(x2, np.exp(x2),'ro')
 
+
+update this code
 
