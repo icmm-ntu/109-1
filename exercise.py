b18b488445f5512f1d1b1928d0563000e23a0f28
icmm-ntu <74669310+icmm-ntu@users.noreply.github.com>
2020-12-03 15:37:06 +0800

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -1,33 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-beta =  1.0 # kg/s
-m = 1.0 # kg
-g = 9.8 # m/s^2
-v0 = 0. # m/s
+omega = 1. #
 
-#h = 0.01
-#N = 1000
-N = 1000
-t_arr = np.linspace(0, 10, N+1)
 
-v_arr_analytic = m* g / beta + (v0 - m*g/beta) * np.exp(-beta/m * t_arr)
+N = 10000
+h = 0.01
+
+t_arr = np.linspace(0,100,N+1)
+x_arr = np.zeros(N+1)
+v_arr = np.zeros(N+1)
+
+v0 = 0
+#x0 = 1.
+
+x_arr[0] = 1. #amplitude of oscillation
+v_arr[0] = 0. # initial velocity
+
+for i in range(N):
+    #x_new = x_old + K1_x = x_old + h * v_old
+    x_arr[i+1] = x_arr[i] + h * v_arr[i]
+    #v_new = v_old + K1_v = v_old + h* (- omega**2 * x_old)
+    v_arr[i+1] = v_arr[i] + h *  (- omega**2 * x_arr[i])
+    
+    
+x_arr_analytical = 1. * np.cos(omega * t_arr)
 
 fig = plt.figure( dpi = 70 ) # dots per inch
 fig.set_size_inches(6,4)
-
-plt.plot(t_arr, v_arr_analytic,'-k',label = 'Analytic Solution', lw = 5)
-
-plt.xlabel(r'$t\;(\rm sec)$',fontsize = 20)
-plt.ylabel(r'$v\;(\rm m/s)$', fontsize = 20)
-#plt.tight_layout()
-#===
-v_arr = np.zeros(N+1)
-v_arr[0] = 0.
-for i in range(N):
-    v_arr[i+1] = v_arr[i] + 0.01 * (g - beta*v_arr[i]/m)
-
-plt.plot(t_arr, v_arr, '--r',label = 'Numerical Solution')   
-plt.xlabel(r'$t\;(\rm sec)$',fontsize = 20)
-plt.ylabel(r'$v\;(\rm m/s)$', fontsize = 20)
-
+plt.plot(t_arr, x_arr_analytical, 'k-', lw = 2, label = "Analytical Solution")
+plt.plot(t_arr, x_arr, 'r--', label = "Euler Method")
