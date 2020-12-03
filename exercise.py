0dea80f0dbba3868be1700398f97f05ef523ecc0
icmm-ntu <74669310+icmm-ntu@users.noreply.github.com>
2020-12-03 16:54:11 +0800

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -1,50 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-omega = 1. #
+alpha = 1.
+Lx = 1.
+Nx = 100
+hx = 0.01 # meter, spatial separation 
+ht = 0.01 # seconds, time separation. 
+x_arr = np.linspace(0,1, 101)   
+#t_arr = np.linspace(0, 1, 101)
+#PI = np.pi
+PI = 3.14
 
-
-N = 10000
-h = 0.01
-
-t_arr = np.linspace(0,100,N+1)
-x_arr = np.zeros(N+1)
-v_arr = np.zeros(N+1)
-
-v0 = 0
-#x0 = 1.
-
-
-def ComputeK(x,v,h):
-    fx = v
-    fv = -omega **2. * x
-    K_x = h * fx
-    K_v = h * fv
-    return [K_x, K_v]
-
-
-x_arr[0] = 1. #amplitude of oscillation
-v_arr[0] = 0. # initial velocity
-
-for i in range(N):
-    #x_new = x_old + K1_x = x_old + h * v_old
-    [K1_x,K1_v] = ComputeK(x_arr[i], v_arr[i], h )
-    x_mid = x_arr[i] + 0.5 * K1_x
-    v_mid = v_arr[i] + 0.5 * K1_v
-    #======
-    [K2_x,K2_v] = ComputeK(x_mid, v_mid, h )
-    x_arr[i+1] = x_arr[i] + K2_x
-    v_arr[i+1] = v_arr[i] + K2_v
-    
-    
-x_arr_analytical = 1. * np.cos(omega * t_arr)
+#=====
 fig = plt.figure( dpi = 70 ) # dots per inch
 fig.set_size_inches(6,4)
-plt.plot(t_arr, x_arr_analytical, 'k-', lw = 2, label = "Analytical Solution")
-plt.plot(t_arr, x_arr, 'r--', label = "Euler Method")
 
+t_arr = np.array([0,0.05,0.1,0.2,0.5])
 
-   
+for t in t_arr:
+    T_analytical = np.sin(PI * x_arr) * np.exp(-PI**2 * t) + 50.
+    plt.plot(x_arr, T_analytical, '-', label = "time = %5.2f s"%(t))
     
-    
-    
+plt.legend( )
