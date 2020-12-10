f96ecc26430b459b77e8311cdbec3ef459cfe0b2
icmm-ntu <74669310+icmm-ntu@users.noreply.github.com>
2020-12-10 15:13:26 +0800

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -17,7 +17,7 @@ print(x_arr)
 #===== Time Array =====
 h_t = 0.001
 #Nt = 1000
-Nt = 10
+Nt = 100
 total_time = Nt * h_t
 print ("h_t = ", h_t)
 print ("total time = %.2f sec"%total_time)
@@ -33,15 +33,17 @@ T_Data[0,] = 10.* np.sin(PI * x_arr ) + 50. # initial condition.
 
 
 for n in range(0,9):
+    # update T_Data for all points except BCs
     for i in range(1, 20): ## every points from 0 to 20 except boundary points
         #f_n_i = alpha * (T_left + T_right - 2. * T_i)/hx**2
         f_n_i = alpha * (T_Data[n,i-1] + T_Data[n, i+1] - 2. * T_Data[n,i])/h_x**2.
         T_Data[n+1, i] = T_Data[n,i] + h_t * f_n_i
+    #=== update T_Data for BCs
+    T_Data[n+1,0] = 50.
+    T_Data[n+1,20] = 50.
 
-        
-        
-        
-        #===== Plotting =====
+
+#===== Plotting =====
 fig = plt.figure( dpi = 150 ) # dots per inch
 fig.set_size_inches(6,4)
 PI = np.pi 
@@ -58,3 +60,18 @@ plt.xlabel(r'$x\; \rm (m)$')
 plt.ylabel(r'$T\; \rm ( ^\circ C)$')
 plt.legend(loc="upper left")
 plt.tight_layout()
+
+#===== check_Von_Neumann Criterion 
+gamma = alpha * h_t/h_x**2 
+if gamma > 1/2.:
+    print ("Von_Neumann Unstable, gamma = %.2f!!!"%gamma)
+else:
+    print  ("Von_Neumann Stable, gamma = %.2f"%gamma)
+
+plt.xlim(0,1)
+plt.ylim(45,65)
+plt.xlabel(r'$x\; \rm (m)$')
+plt.ylabel(r'$T\; \rm ( ^\circ C)$')
+plt.legend(loc="upper left")
+plt.tight_layout()
+#plt.savefig("Figs/TimeEvolution_HeatEq_beta_0_Euler.png")
