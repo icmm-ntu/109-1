5bdea90fabbdfa02d51d1c9c50e0714a5ed732de
icmm-ntu <74669310+icmm-ntu@users.noreply.github.com>
2020-12-17 14:50:42 +0800

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -1,55 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 plt.close('all')
-alpha = 1.0
-Lx = 1. # meters
-#===== X Array =====
-h_x = 0.05
-Nx = 21
-x_arr = h_x * (np.arange(Nx))
-#===== Time Array =====
-h_t = 0.001
-Nt = 100
-total_time = Nt * h_t
-print ("h_t = ", h_t)
-print ("total time = %.2f sec"%total_time)
-t_arr = h_t * (np.arange(Nt))
-#====
-T_Data = np.zeros([Nt, Nx]) # 10 interations, 21 points
-# Initial Condition
-T1_arr = 50. * x_arr
-T2_arr = 50. * x_arr**2
-T3_arr = -50. * x_arr**2 + 100. * x_arr
-T4_arr = 50 * np.heaviside(x_arr - 0.5, 0.5) 
-T_Data[0,] = T4_arr
-                
+# verlet method 
+x0 = 1.0 # meters
+x1 = 0.2 # meters
+h = 0.01 # seconds
+omega = 2. # Hz
+# spring resotring force 
+a1 = -omega **2 * x1
+x2 = 2*x1 - x0 + a1 * h**2
+Nt = 1000
+t_arr = np.arange(Nt) * h
+x_arr = np.zeros(Nt)
+a_arr = np.zeros(Nt)
+x_arr[0] = x0
+x_arr[1] = x1
+
+#
+for n in range(1,Nt-1):
+    a_arr[n] =  -omega **2 * x_arr[n]
+    x_arr[n+1] = 2*x_arr[n] - x_arr[n-1] + a_arr[n] * h**2
     
-for n in range(0,Nt-1):
-    # update T_Data for all points except BCs
-    for i in range(1, Nx-1): ## every points from 0 to 20 except boundary points
-        f_n_i = alpha * (T_Data[n,i-1] + T_Data[n, i+1] - 2. * T_Data[n,i])/h_x**2.
-        T_Data[n+1, i] = T_Data[n,i] + h_t * f_n_i
-    #=== update T_Data for BCs
-    T_Data[n+1,0] = 0.
-    T_Data[n+1,Nx-1] = 50.
-
-
-
-#===== Plotting =====
-fig = plt.figure( dpi = 150 ) # dots per inch
-fig.set_size_inches(6,4)
-#PI = np.pi 
-#n_list = [0,1,2,3,4,5,6,7,8,9]
-n_list = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 99]
-for n in n_list:
-    t_n = h_t * n
-    plt.plot(x_arr, T_Data[n,:] , '--', label = 'time = %.2f sec'%(t_n))
-
-plt.xlim(0,1)
-plt.ylim(0,55)
-plt.xlabel(r'$x\; \rm (m)$')
-plt.ylabel(r'$T\; \rm ( ^\circ C)$')
-plt.legend(loc="upper left")
-plt.tight_layout()
-
-
+    
+plt.plot(t_arr, x_arr,'--r')
