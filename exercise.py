8752aedc8e0292fb1de7150096f4ca22be052ea5
icmm-ntu <74669310+icmm-ntu@users.noreply.github.com>
2020-12-17 16:04:57 +0800

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -1,25 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
 plt.close('all')
-# verlet method 
+# Velocity-Verlet method 
+m = 1.0 # kg
+F = 10. # kg * m/s^2
+w0 = 1.0 # rad/s
+w1 = 2.0 #rad/s
 x0 = 1.0 # meters
-x1 = 0.2 # meters
+v0 = 1.0 # m/s
 h = 0.01 # seconds
-omega = 2. # Hz
-# spring resotring force 
-a1 = -omega **2 * x1
-x2 = 2*x1 - x0 + a1 * h**2
 Nt = 1000
 t_arr = np.arange(Nt) * h
-x_arr = np.zeros(Nt)
-a_arr = np.zeros(Nt)
-x_arr[0] = x0
-x_arr[1] = x1
 
-#
-for n in range(1,Nt-1):
-    a_arr[n] =  -omega **2 * x_arr[n]
-    x_arr[n+1] = 2*x_arr[n] - x_arr[n-1] + a_arr[n] * h**2
+term1 = x0 * np.cos(w0 * t_arr) 
+term2 = v0/w0*np.sin(w0 * t_arr)
+term3 = F/(m*(w0)**2.-w1**2.)*(np.cos(w1*t_arr)-np.cos(w0*t_arr))
+ 
+x_analytic = term1+ term2+term3
+plt.plot(t_arr, x_analytic, '-k', label="Analytic Solution")
+
+
+# Velocity Verlet Method
+#Nt = 1000
+x_arr = np.zeros(Nt)
+v_arr = np.zeros(Nt)
+a_arr = np.zeros(Nt)
+
+x_arr[0] = x0
+v_arr[0] = v0
+a_arr[0] = -w0**2.* x_arr[0] + F/m * np.cos(w1 * t_arr[0])
+
+for n in range(0, Nt-1):
+    #Step 1
+    x_arr[n+1] = x_arr[n] + h * v_arr[n] + 0.5 * a_arr[n] * h**2.
+    # Step 2
+    a_arr[n+1] = -w0**2.*x_arr[n+1] + F/m * np.cos(w1 * t_arr[n+1])
+    # Step 3
+    v_arr[n+1] = v_arr[n] + 0.5 * (a_arr[n] + a_arr[n+1]) * h 
     
     
-plt.plot(t_arr, x_arr,'--r')
+plt.plot(t_arr, x_arr, '--r', label="Velocity Verlet Solution")
+
+plt.legend()
+
